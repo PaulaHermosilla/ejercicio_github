061da2b7307a6785c2d361fafa701c735cccad14
Paula <paula.hermosilla@alumni.mondragon.edu>
2020-04-24 09:12:32 +0200

    Modifico Carga ficheros.R

--- Carga ficheros.R
+++ Carga ficheros.R
@@ -1,4 +1,5 @@
-setwd("C:/Users/Paula/Desktop/Ejercicio-git")
+#este es el preograma de carga de datos
+setwd("C:/Users/Paula/Desktop/Ejercicio-git/Version2")
 a<-read.csv('time_series_19-covid-Confirmed.csv')
 b<-read.csv('time_series_19-covid-Deaths.csv')
 c<-read.csv('time_series_19-covid-Recovered.csv')
