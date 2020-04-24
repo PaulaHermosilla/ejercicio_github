2b172b7d46ce8eb17f303d257d599f812c3928a8
Paula <paula.hermosilla@alumni.mondragon.edu>
2020-04-24 09:02:11 +0200

    Añado los ficheros originales

---
+++ Carga ficheros.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/Paula/Desktop/Ejercicio-git")
+a<-read.csv('time_series_19-covid-Confirmed.csv')
+b<-read.csv('time_series_19-covid-Deaths.csv')
+c<-read.csv('time_series_19-covid-Recovered.csv')
+  
